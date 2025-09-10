9ffb4947ed3c3c45d4868d01f23c75cc161017d0
Kiing Ncube <maphosanesbert@gmail.com>
2025-09-10 11:40:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,1 @@
-// Update footer year
-document.getElementById("year").textContent = new Date().getFullYear();
 
-  const form = document.getElementById("contactForm");
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const data = new FormData(form);
-    const response = await fetch(form.action, {
-      method: form.method,
-      body: data,
-      headers: { 'Accept': 'application/json' }
-    });
-    if (response.ok) {
-      alert("🎉 Thank you! Your message has been sent.");
-      form.reset();
-    } else {
-      alert("❌ Oops! Something went wrong, please try again.");
-    }
-  });
